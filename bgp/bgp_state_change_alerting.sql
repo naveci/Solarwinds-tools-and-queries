c899fc9bd12313e6fb829e41ff5c8675fd7a929c
Axel Robbe <22393597+naveci@users.noreply.github.com>
2019-04-19 09:11:45 +0200

    Create bgp_state_change_alerting.sql

---
+++ bgp/bgp_state_change_alerting.sql
@@ -0,0 +1,19 @@
+--- This query is designed for usage an alert such as an email.
+
+SELECT Nodes.Caption AS [Device], Nodes.DetailsURL AS [_LinkFor_Device]
+,'/Orion/images/StatusIcons/Small-' + StatusIcon AS [_IconFor_Device]
+, Nodes.IP_Address as [IP], Nodes.DetailsURL as [_LinkFor_IP]
+, RN.ProtocolName AS Protocol
+, (SELECT TOP 1 InterfaceIndex FROM Orion.Routing.RoutingTable AS rt WHERE rt.NodeID = RN.NodeID AND rt.RouteNextHop = RN.NeighborIP) AS InterfaceDescription
+, RN.NeighborIP, RN.Ip_Address_Type AS IPversion, RN.IsDeleted
+, CONCAT(HOURDIFF(tolocal(RN.LastChange),getdate())/24,' Day(s) ',
+        HOURDIFF(tolocal(RN.LastChange),getdate())-(HOURDIFF(tolocal(RN.LastChange),getdate())/24)*24,'h ',
+        MINUTEDIFF(tolocal(RN.LastChange),getdate())-(MINUTEDIFF(tolocal(RN.LastChange),getdate())/60)*60,'m') AS Uptime
+, RN.AutonomousSystem AS BGP_AS, RN.ProtocolStatusDescription AS Status
+FROM Orion.Routing.Neighbors AS RN
+LEFT JOIN Orion.Nodes AS Nodes ON RN.NodeID = Nodes.NodeID
+
+WHERE ProtocolName LIKE '%BGP%'
+AND HOURDIFF(tolocal(RN.LastChange),getdate()) < 72
+
+ORDER BY RN.LastChange DESC
